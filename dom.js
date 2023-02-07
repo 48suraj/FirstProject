dc7f595b143267a082642245fcd5f7f7e352907d
48suraj <48suraj>
2023-02-07 14:45:00 +0530

    dom2

--- dom.js
+++ dom.js
@@ -13,6 +13,14 @@ header.style.borderBottom= 'solid 3px #000';
 var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[4]);
-items[4].style.fontWeight ='bold';
-items[4].style.fontColor ='green';
-//items[4].style.backgroundColor='yellow';
+// items[0].style.fontWeight ='bold';
+// items[1].style.fontWeight ='bold';
+// items[2].style.fontWeight ='bold';
+// items[3].style.fontWeight ='bold';
+// items[4].style.fontWeight ='bold';
+//items[4].style.fontColor ='green';
+items[3].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold";
+
+}
