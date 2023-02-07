ea755bb692401c600e6e38046a48f9a91f0cdc2a
48suraj <48suraj>
2023-02-07 19:36:45 +0530

    query

--- dom.js
+++ dom.js
@@ -6,28 +6,35 @@
 // console.log(document.all);
 // document.all[10].textContent='hello';
 // document.all[22].textContent.fontcolor ='green';
-var headerTitle=document.getElementById('header-title');
-var header=document.getElementById('main-header')
-header.style.borderBottom= 'solid 3px #000';
+// var headerTitle=document.getElementById('header-title');
+// var header=document.getElementById('main-header')
+// header.style.borderBottom= 'solid 3px #000';
 
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[4]);
-// items[0].style.fontWeight ='bold';
-// items[1].style.fontWeight ='bold';
-// items[2].style.fontWeight ='bold';
-// items[3].style.fontWeight ='bold';
-// items[4].style.fontWeight ='bold';
-//items[4].style.fontColor ='green';
-items[3].style.backgroundColor='green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold";
-
-}
-// var items=document.getElementsByClassName('suraj-group-item');
+// var items=document.getElementsByClassName('list-group-item');
 // console.log(items);
-// items[0].style.fontWeight='bold';
-// items[0].style.backgroundColor='yellow';
+// console.log(items[4]);
+// // items[0].style.fontWeight ='bold';
+// // items[1].style.fontWeight ='bold';
+// // items[2].style.fontWeight ='bold';
+// // items[3].style.fontWeight ='bold';
+// // items[4].style.fontWeight ='bold';
+// //items[4].style.fontColor ='green';
+// items[3].style.backgroundColor='green';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold";
 
-var li=document.getElementsByTagName('li');
-li[5].style.backgroundColor='green';
+// }
+// // var items=document.getElementsByClassName('suraj-group-item');
+// // console.log(items);
+// // items[0].style.fontWeight='bold';
+// // items[0].style.backgroundColor='yellow';
+
+// var li=document.getElementsByTagName('li');
+// li[5].style.backgroundColor='green';
+
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+ secondItem.style.backgroundColor='green';
+
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility='hidden';
+
